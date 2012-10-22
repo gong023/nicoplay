a28f887979c9049e86e1c1893308a4957ef305af
gong023 <gon.gong.gone@gmail.com>
2012-10-23 00:19:15 +0900

    doing

--- public/js/nicoaudio.js
+++ public/js/nicoaudio.js
@@ -15,8 +15,11 @@ var NicoPlayer = function(){
       if (current_audio === null) {
         alert('audio is not foud');
       }
-      date = nicoutil.parseDate(ctime);
+      location.href = "#pre_player";
+      var date = nicoutil.parseDate(ctime);
       current_audio.src = domain + "public/audio/all/" + date + "/" + video_id + ".mp3";
+      //current_audio.src = "http://taira-komori.jpn.org/sound/gamesf01/Surprise1.mp3";
+      console.log(current_audio.src);
       current_audio.play();
       playlist.init(current_playing);//やだ
     },
@@ -24,11 +27,13 @@ var NicoPlayer = function(){
       if (current_audio === null) {
         alert('audio is not found');
       }
+      location.href = "#playlist";
       current_audio.pause();
     },
     'playNext' :function() {
+      location.href = "#post_player";
       var next = playlist.next();
-      this.play(next.ctime, next.video_id, playlist.getIndex());
+      audio.play(next.ctime, next.video_id, playlist.getIndex());
     }
   };
 };
