5bce6d736548c2e63706f30b637252fe762910fd
gong023 <gon.gong.gone@gmail.com>
2012-11-13 01:20:15 +0900

    地下鉄でも大丈夫！

--- public/js/nicoaudio.js
+++ public/js/nicoaudio.js
@@ -1,5 +1,8 @@
 var NicoPlayer = function(){
   var current_audio = null;
+  var prepare_timer = null;
+  var next_audio = null;
+  var do_next = true;
   var url = 'http://gong023.com/nicoplay/';
   var my_view = NicoView();
   return {
@@ -10,21 +13,41 @@ var NicoPlayer = function(){
       }
       $.mobile.defaultPageTransition = 'slide';
       current_audio.autoplay = false;
-      current_audio.controls = true;
       current_audio.addEventListener('ended', this.playNext);
       Playlist.init(index);
-      var playing = Playlist.getPlaying();
+      var playing = Playlist.getPlaying(Playlist.getIndex());
       my_view.togglePlayer(playing.title);
       this.play();
+      if (prepare_timer == null) {
+        setInterval("MyAudio.prepareNext()", 1500);
+      }
     },
-    'getSrc' :function() {
-      var playing = Playlist.getPlaying();
+    'prepareNext' :function() {
+      console.log('preparing');
+      if (do_next === false) {
+        console.log('prepared');
+        console.log(next_audio);
+        return;
+      }
+      var next_index = nicoutil.addStrings(Playlist.getIndex(), 1);
+      next_audio = new Audio(this.getSrc(next_index));
+      next_audio.autoplay = false;
+      do_next = false;
+    },
+    'getSrc' :function(index) {
+      index = index || Playlist.getIndex();
+      var playing = Playlist.getPlaying(index);
       var date = nicoutil.parseDate(playing.ctime);
       return url + "public/audio/all/" + date + "/" + playing.video_id + ".mp3";
     },
     'play' :function() {
       if (current_audio === null) {
         alert('audio is not foud');
+      }
+      if (next_audio !== null && next_audio.src == this.getSrc()) {
+        current_audio = next_audio;
+        current_audio.play();
+        return;
       }
       current_audio.src = this.getSrc();
       //current_audio.src = "http://taira-komori.jpn.org/sound/gamesf01/Surprise1.mp3";
@@ -40,6 +63,9 @@ var NicoPlayer = function(){
     'playNext' :function() {
       var next = Playlist.next();
       my_view.togglePlayer(next.title);
+      if (next_audio.src == MyAudio.getSrc()) {
+        do_next = true;
+      }
       MyAudio.play();
     }
   };
@@ -52,8 +78,11 @@ var NicoPlaylist = function(){
       list = JSON.parse(document.player.playlist.value);
       index = current_playing;
     },
-    'getPlaying' :function(){
-      return list[index];
+    'getIndex' :function() {
+      return index;
+    },
+    'getPlaying' :function(list_index){
+      return list[list_index];
     },
     'next' :function() {
       var list_length = 0;
@@ -88,6 +117,11 @@ var NicoView = function() {
 var nicoutil = {
   'parseDate' :function(date) {
     return date.match(/^[0-9]+-[0-9]+-[0-9]+/);
+  },
+  'addStrings' :function(x, y) {
+    x = parseInt(x);
+    y = parseInt(y);
+    return String(x + y);
   }
 };
 (function() {
