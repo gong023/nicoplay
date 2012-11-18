d21289c7d924fbe0ba9f218fdb3da9df9c6c6dc8
gong023 <gon.gong.gone@gmail.com>
2012-11-18 21:08:41 +0900

    色々バグとったりした

--- public/js/nicoaudio.js
+++ public/js/nicoaudio.js
@@ -11,7 +11,6 @@ var NicoPlayer = function(){
       if (! current_audio.canPlayType) {
         alert('browser does not support');
       }
-      $.mobile.defaultPageTransition = 'slide';
       current_audio.autoplay = false;
       Playlist.init(index);
       var playing = Playlist.getPlaying(Playlist.getIndex());
@@ -19,7 +18,7 @@ var NicoPlayer = function(){
       my_view.displayThumbnail(playing.video_id);
       this.play();
       if (prepare_timer == null) {
-        setInterval("MyAudio.prepareNext()", 1500);
+        setInterval("MyAudio.prepareNext()", 4000);
       }
     },
     'prepareNext' :function() {
@@ -27,8 +26,10 @@ var NicoPlayer = function(){
         return;
       }
       var next_index = nicoutil.addStrings(Playlist.getIndex(), 1);
-      next_audio = new Audio(this.getSrc(next_index));
+      var src = (this.getSrc(next_index) != undefined) ? this.getSrc(next_index) : this.getSrc(0);
+      next_audio = new Audio(src);
       next_audio.autoplay = false;
+      next_audio.preload = 'auto';
       do_next = false;
     },
     'getSrc' :function(index) {
@@ -40,6 +41,7 @@ var NicoPlayer = function(){
     'play' :function() {
       if (current_audio === null) {
         alert('audio is not foud');
+        return;
       }
       if (next_audio !== null && next_audio.src == this.getSrc()) {
         current_audio = next_audio;
@@ -47,26 +49,35 @@ var NicoPlayer = function(){
         current_audio.src = this.getSrc();
       }
       //current_audio.src = "http://taira-komori.jpn.org/sound/gamesf01/Surprise1.mp3";
-      current_audio.play();
       current_audio.addEventListener('ended', this.playNext);
+      current_audio.play();
     },
     'pause' :function(go_back) {
-      if (current_audio === null) {
+      if (current_audio === null || !current_audio.canPlayType) {
         alert('audio is not found');
-      }
-      var go_back = go_back || false;
-      if (go_back) {
-        location.href = "#playlist";
+        my_view.backToPlaylist();
+        return;
       }
       current_audio.pause();
+      var go_back = go_back || false;
+      if (go_back) {
+        my_view.backToPlaylist();
+      }
     },
     'playNext' :function() {
       var next = Playlist.next();
       my_view.togglePlayer(next.title);
       my_view.displayThumbnail(next.video_id);
-      if (next_audio.src == MyAudio.getSrc()) {
+      if (next_audio !== null && next_audio.src == MyAudio.getSrc()) {
         do_next = true;
       }
+      MyAudio.play();
+    },
+    'playPrevious' :function() {
+      var previous = Playlist.previous();
+      var reverse = true;
+      my_view.togglePlayer(previous.title, reverse);
+      my_view.displayThumbnail(previous.video_id);
       MyAudio.play();
     }
   };
@@ -74,6 +85,12 @@ var NicoPlayer = function(){
 var NicoPlaylist = function(){
   var list;
   var index;
+  var shuffle = null;
+  var getListLength = function() {
+    var list_length = 0;
+    for (var name in list) list_length++;
+    return list_length;
+  };
   return {
     'init' :function(current_playing) {
       list = JSON.parse(document.player.playlist.value);
@@ -86,34 +103,53 @@ var NicoPlaylist = function(){
       return list[list_index];
     },
     'next' :function() {
-      var list_length = 0;
-      for (var name in list) list_length++;
-      if (index >= list_length - 1) {
+      index++;
+      if (index >= getListLength()) {
         index = 0;
         return list[0];
       }
-      index++;
       return list[index];
+    },
+    'previous' :function() {
+      index--;
+      if (index < 0) {
+        var last = getListLength() - 1;
+        index = last;
+        return list[last];
+      }
+      return list[index];
+    },
+    'shuffle' :function() {
+      var length = getListLength();
+      for (var i; i < length; i++) {
+      }
     }
   };
 };
 var NicoView = function() {
   var domain = 'http://gong023.com:4567/nicoplay/';
-  //var thumbnail_domain = 'http://tn-skr1.smilevideo.jp/smile?i=18267644';
   var thumbnail_domain = 'http://tn-skr1.smilevideo.jp/smile';
+  var changeLocation = function(ref, animation, is_reverse) {
+    $.mobile.defaultPageTransition = 'slide';
+    $.mobile.changePage(ref, {transition: animation, reverse: is_reverse});
+  }
   return {
-    'togglePlayer' :function(title) {
+    'togglePlayer' :function(title, reverse) {
+      var reverse = reverse || false;
       if (location.href == domain + '#pre') {
         document.getElementById('post_title').innerHTML = title;
-        this.changeLocation("#post", "slide");
+        changeLocation("#post", "slide", reverse);
       } else {
         document.getElementById('pre_title').innerHTML = title;
-        this.changeLocation("#pre", "slide");
+        changeLocation("#pre", "slide", reverse);
       }
     },
-    'changeLocation' :function(ref, animation) {
-      $.mobile.defaultPageTransition = 'slide';
-      $.mobile.changePage(ref, animation, true, true);
+    'backToPlaylist' :function() {
+        changeLocation('#playlist', 'slide', true);
+        var post = document.getElementById('post_thumbnail_area');
+        var pre = document.getElementById('pre_thumbnail_area');
+        if (post.hasChildNodes()) post.removeChild(post.firstChild);
+        if (pre.hasChildNodes()) pre.removeChild(pre.firstChild);
     },
     'displayThumbnail' :function(video_id) {
       var video_id = video_id.replace(/^sm/, '');
@@ -125,18 +161,13 @@ var NicoView = function() {
       if (location.href == domain + '#pre') {
         img.setAttribute('id', 'pre_thumbnail');
         document.getElementById('pre_thumbnail_area').appendChild(img);
-        var remove = document.getElementById('post_thumbnail');
-        if (remove) {
-          document.getElementById('post_thumbnail_area').removeChild(remove);
-        }
+        var remove = document.getElementById('post_thumbnail_area');
       } else {
         img.setAttribute('id', 'post_thumbnail');
         document.getElementById('post_thumbnail_area').appendChild(img);
-        var remove = document.getElementById('pre_thumbnail');
-        if (remove) {
-          document.getElementById('pre_thumbnail_area').removeChild(remove);
-        }
+        var remove = document.getElementById('pre_thumbnail_area');
       }
+      if (remove.hasChildNodes()) remove.removeChild(remove.firstChild);
     }
   };
 }
