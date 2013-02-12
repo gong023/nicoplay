4a004d4964f215136dcafbfd978e011c85e0e193
gong023 <gon.gong.gone@gmail.com>
2013-02-12 23:41:39 +0900

    delete indeed file

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-open('/etc/my/sinatra/nicoplay/env.txt').each do |env|
+File::open('/etc/my/sinatra/nicoplay/env.txt').each do |env|
   if env.gsub(/\n/, "") == 'sakura'
     require File.expand_path(File.dirname(__FILE__)) + '/app/sakura/app.rb'
   elsif env.gsub(/\n/, "") == 'ec2'
