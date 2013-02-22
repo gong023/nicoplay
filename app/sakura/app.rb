8910b75ea1e0bba70b27dc2cfe203976203365c5
gong023 <gon.gong.gone@gmail.com>
2013-02-22 22:43:57 +0900

    一時間ごとにとるようにしたので

--- app/sakura/app.rb
+++ app/sakura/app.rb
@@ -11,12 +11,10 @@ class Nicoplay < Sinatra::Base
   #set :port, 80
 
   get '/nicoplay/' do
-    # 裏で走るscriptと整合性をとる
-    # AM0:00 ~ 4:00は次の日の分とらない
-    @to_date = Time.now.strftime("%H") <= '04' ? Date::today - 1 : Date::today
-    @from_date = @to_date - 30
+    @to_date = Date::today
+    @from_date = Date::today - 30
     @ranking = {}
-    NicoRankingWWW.new.getRankingByinterval(@from_date, @to_date).each_with_index do |ret, i|
+    NicoRankingWWW.new.getRankingByinterval(@from_date, Time.now).each_with_index do |ret, i|
       ret['ctime'] = ret['ctime'].to_s.sub(/\ [0-9]+\:[0-9]+\:[0-9]+ \+[0-9]+$/,  '')
       @ranking.store(i, ret)
     end
