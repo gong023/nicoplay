477ff4e428f344b81154d33308da5adf8ac1ae23
gong023 <gon.gong.gone@gmail.com>
2013-03-12 09:53:04 +0900

    あわせた

--- app/ec2/app.rb
+++ app/ec2/app.rb
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 require 'sinatra/base'
 require 'haml'
 require "#{Dir::pwd}/class/nicoranking_www.rb"
@@ -7,13 +8,26 @@ class Nicoplay < Sinatra::Base
   set :haml, :format => :html5
 
   get '/' do
-    @to_date = Date::today
-    @from_date = Date::today - 30
-    @ranking = {}
-    NicoRankingWWW.new.getRankingByinterval(@from_date, Time.now).each_with_index do |ret, i|
-      ret['ctime'] = ret['ctime'].to_s.sub(/\ [0-9]+\:[0-9]+\:[0-9]+ \+[0-9]+$/,  '')
-      @ranking.store(i, ret)
-    end
+    @from_date = (Date::today - 30).to_s
+    @to_date = Date::today.to_s
+    @ranking = NicoRankingWWW.new.getByInterval(@from_date, Time.now)
     haml :rank
   end
+
+  get '/keyword' do
+    keyword = params[:w]
+    @ranking = NicoRankingWWW.new.getByKeyword(keyword)
+    @to_date = @ranking[0]['ctime']
+    @from_date = @ranking[@ranking.length-1]['ctime']
+    haml :rank
+  end
+
+  get '/rand' do
+    limit = params[:l].nil? ? 30 : params[:l]
+    @ranking = NicoRankingWWW.new.getByRand(limit)
+    @to_date = @ranking[0]['ctime']
+    @from_date = @ranking[@ranking.length-1]['ctime']
+    haml :rank
+  end
+
 end
